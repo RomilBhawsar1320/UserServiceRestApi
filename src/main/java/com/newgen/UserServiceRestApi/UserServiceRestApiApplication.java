04cbc0bc559661d9c14f0ef4f15992ab363d0776
Romil Bhawsar <romilbhawsar20@gmail.com>
2025-03-14 00:58:07 +0530

    application.properties added
    added from product service

---
+++ src/main/java/com/newgen/UserServiceRestApi/UserServiceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.newgen.UserServiceRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newgen/UserServiceRestApi/UserServiceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgen.UserServiceRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
